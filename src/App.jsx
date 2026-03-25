ce1f57b4072daf3d3d66f227445503bb8fd16bbe
melt71 <blinkboy20073@gmail.com>
2026-03-25 20:15:48 +0530

    "/"

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename="/AiSearcho/">
+    <BrowserRouter basename="/">
       <UserSync />
       <div className="main">
         <Routes>
