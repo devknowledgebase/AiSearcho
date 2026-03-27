3d37ec71e5b270eeab98211a49fa64c45d5a1ed6
melt71 <blinkboy20073@gmail.com>
2026-03-27 20:45:24 +0530

    start for free interactivity added #1

--- src/Framer.jsx
+++ src/Framer.jsx
@@ -41,7 +41,14 @@ export default function Framer() {
      <div className='hero-text'>
       <h1>Boost your rankings with AI.</h1>
       <p>Elevate your site’s visibility effortlessly with AI, where smart technology meets user-friendly SEO tools.</p>
-      <button>Start for free</button>
+      <button onClick={() => {
+              setMenuOpen(false)
+              if (user) {
+                navigate('/dashboard')
+              } else {
+                openSignIn({ forceRedirectUrl: '/dashboard', fallbackRedirectUrl: '/dashboard' })
+              }
+            }}>Start for free</button>
      </div>
     </div> 
      <div className='hero-img'>
